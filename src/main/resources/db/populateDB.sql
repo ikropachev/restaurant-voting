c1b62fa589752d51377f09120aec97ef493046a3
Rock133 <paporotnik796@gmail.com>
2021-11-09 22:45:31 +0300

    add_index_and_populate_postgresql

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -15,12 +15,12 @@ INSERT INTO USERS (NAME, EMAIL, PASSWORD, PRIVILEGED) VALUES
     ('Second_user', 'second@gmail.com', 'second_pass', FALSE);          -- 100006
 
 INSERT INTO DISHES (NAME, PRICE, RESTAURANT_ID) VALUES
-    ('Meat Burger', 100, 100001),                                       -- 100007
-    ('Vegan Burger', 150, 100001),                                      -- 100008
-    ('Chicken Burger', 50, 100001),                                     -- 100009
-    ('Meat Shaverma', 100, 100002),                                     -- 100010
-    ('Vegan Shaverma', 150, 100002),                                    -- 100011
-    ('Chicken Shaverma', 50, 100002),                                   -- 100012
-    ('Hachapuri po adzharski', 350, 100003),                            -- 100013
-    ('Hachapuri po megrelski', 450, 100003),                            -- 100014
-    ('Hachapuri po imeretinski', 550, 100003);                          -- 100015
+    ('Meat Burger', 100, 100001),                                      -- 100007
+    ('Vegan Burger', 150, 100001),                                     -- 100008
+    ('Chicken Burger', 50, 100001),                                    -- 100009
+    ('Meat Shaverma', 100, 100002),                                    -- 100010
+    ('Vegan Shaverma', 150, 100002),                                   -- 100011
+    ('Chicken Shaverma', 50, 100002),                                  -- 100012
+    ('Hachapuri po adzharski', 350, 100003),                           -- 100013
+    ('Hachapuri po megrelski', 450, 100003),                           -- 100014
+    ('Hachapuri po imeretinski', 550, 100003);                         -- 100015
