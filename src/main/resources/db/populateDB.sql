e67f47ccc654c894d608b18e13b4645e08ee15c4
Rock133 <paporotnik796@gmail.com>
2021-11-09 22:46:22 +0300

    add_index_and_populate_postgresql

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -4,15 +4,15 @@ DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO RESTAURANTS (NAME) VALUES
-    ('Nowhere'),                                                        -- 100000
-    ('Burgers'),                                                        -- 100001
-    ('HappyShaverma'),                                                  -- 100002
-    ('Kavkaz');                                                         -- 100003
+    ('Nowhere'),                                                       -- 100000
+    ('Burgers'),                                                       -- 100001
+    ('HappyShaverma'),                                                 -- 100002
+    ('Kavkaz');                                                        -- 100003
 
 INSERT INTO USERS (NAME, EMAIL, PASSWORD, PRIVILEGED) VALUES
-    ('Admin', 'admin@gmail.com', 'admin_pass', TRUE),                   -- 100004
-    ('First_user', 'first@gmail.com', 'first_pass', FALSE),             -- 100005
-    ('Second_user', 'second@gmail.com', 'second_pass', FALSE);          -- 100006
+    ('Admin', 'admin@gmail.com', 'admin_pass', TRUE),                  -- 100004
+    ('First_user', 'first@gmail.com', 'first_pass', FALSE),            -- 100005
+    ('Second_user', 'second@gmail.com', 'second_pass', FALSE);         -- 100006
 
 INSERT INTO DISHES (NAME, PRICE, RESTAURANT_ID) VALUES
     ('Meat Burger', 100, 100001),                                      -- 100007
